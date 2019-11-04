7b483432999b8ec53fdc62c5c839254cdb0403db
Пользователь <majorov@rodzeta.ru>
2019-11-04 23:03:48 +0300

    Допиливаю лк организатора

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-//Слайдер
+
+//Главная страница
+
+  //Слайдер
   $('.slider_asis').slick({
 	  dots: true,
 	  arrows: true,
@@ -8,34 +11,84 @@ $(document).ready(function() {
 	  autoplaySpeed: 7000,
   });
 
+//Каталог мероприятий
 
-//Календарь
+  //Выпадашки фильтра
+  $('.show_filter').on('click',function() {
+      if($(this).next().is(':visible')) {
+          $(this).next().fadeOut();
+      } else {
+          $('.filter_val').not(this).hide();
+          $(this).next().fadeIn();
+      }
+  });
 
-	//Главная
-	$('.calendar_btn').on('click',function() {
-		if($(this).next().is(':visible')) {
-			$(this).next().fadeOut();
-			$(this).attr('class','calendar_vdwn');
-		} else {
-			$(this).next().fadeIn();
-			$(this).attr('class','calendar_vup');
-		}
+// Скрываем фильтры при клике на пустое место, не работает с датой
+//  $(document).on('click', function(e) {
+//    if (!$(e.target).closest('.catalog_filter_container').length) {
+//      $('.filter_val').fadeOut();
+//    }
+//    e.stopPropagation();
+//  });
+
+
+
+  //Фильтр цена
+	 $('#catalog_filter_price').ionRangeSlider && $('#catalog_filter_price').ionRangeSlider({
+		type: 'double',
+		min: 0,
+		max: 10000,
+		from: 0,
+		to: 10000
 	});
 
+  $('#mobile_catalog_filter_price').ionRangeSlider && $('#mobile_catalog_filter_price').ionRangeSlider({
+   type: 'double',
+   min: 0,
+   max: 10000,
+   from: 0,
+   to: 10000
+  });
 
-	pickmeup('.single', {
-	  flat : true
+  //Фильтр площадки
+	$(function() {
+		$(".catalog_playgrounds_values").niceScroll({
+			cursoropacitymin: 0.5,
+			cursoropacitymax: 1,
+			cursorcolor:"#F69707",
+			cursorwidth: "5px",
+			cursorminheight: 30,
+			cursorborder: "0",
+			cursorborderradius: "5px",
+			});
 	});
 
-	//Каталог фильтр
-	pickmeup('.catalog_single', {
-	  flat : true
-	});
+  //Мобильный фильтр
+  $('.filter_mobile_btn').click (function() {
+  	$('.popup_filter').fadeIn();
+  	$('body').addClass('stop-scrolling')
+  })
+  $('.popup_filter_close').click (function() {
+  	$('.popup_filter').fadeOut();
+  	$('body').removeClass('stop-scrolling')
+  })
 
-	//Календарь страница мероприятия
-	
+  //Раскрывашки мобильного фильтра
+  $('.popup_show_filter').on('click',function() {
+      if($(this).next().is(':visible')) {
+          $(this).next().hide();
+      } else {
+          $('.popup_filter_val').not(this).hide();
+  		    $(this).next().show();
+      }
+  });
+
+// Страница мероприятия
+
+  //Календарь страница мероприятия
+
 	$(function(){
-		
+
 		$.datepicker.regional['ru'] = {
 			closeText: 'Закрыть',
 			prevText: 'Пред.',
@@ -54,133 +107,277 @@ $(document).ready(function() {
 			yearSuffix: ''
 		};
 		$.datepicker.setDefaults($.datepicker.regional['ru']);
-		
+
 		var date = new Date();
-		date.setDate(date.getDate() + 1);
-		
-		
+		date.setDate(date.getDate());
+
+
 		$("#datepicker").datepicker({
 			minDate: date
 		});
 	});
-	
-	//Каталог Фильтр прайс 
-		 $('#catalog_filter_price').ionRangeSlider && $('#catalog_filter_price').ionRangeSlider({
-			type: 'double',
-			min: 0,
-			max: 10000,
-			from: 0,
-			to: 10000
-		});
 
-	//Каталог кастомная прокрутка блока
-	$(function() {  
-		$(".catalog_playgrounds_values").niceScroll({
-			cursoropacitymin: 0.5,
-			cursoropacitymax: 1,
-			cursorcolor:"#F69707",
-			cursorwidth: "5px",
-			cursorminheight: 30,
-			cursorborder: "0",
-			cursorborderradius: "5px",
-			});
-	});
-	
-	
-	//Каталог читать больше отзывы
-	$('.event_page_feedback').readmore({
-		collapsedHeight: 100,
-		embedCSS: false,
-		moreLink: '<a class="top_20">Читать полностью</a>',
-		lessLink: '<a class="top_20">Скрыть</a>'
-	});
+  //Читать больше отзывы
+  $('.event_page_feedback').readmore({
+    collapsedHeight: 100,
+    embedCSS: false,
+    moreLink: '<a class="top_20">Читать полностью</a>',
+    lessLink: '<a class="top_20">Скрыть</a>'
+  });
 
-	//Страницы события читать больше описание
+//Страница организатора
+
+	//Читать больше описание
 	$('.organizer_page_description').readmore({
 		collapsedHeight: 190,
 		embedCSS: false,
 		moreLink: '<a class="top_20">Читать полностью</a>',
 		lessLink: '<a class="top_20">Скрыть</a>'
 	});
-	
-//Плавная прокрутка в топ
-(function($) {
-$(function() {
-
-	$('.page_up').click(function() {
-		$('html, body').animate({scrollTop: 0},500);
-		return false;
-	})
-
-})
-})(jQuery)
 
 
-//Временное показать еще
+//Раздел пользователя
 
-$('.show_more_btn').click (function() {
-	$('.show_more_data').fadeIn();
-})
+  //Меню
+  $('.user_accaunt_menu_show').on('click',function() {
+      if($(this).next().is(':visible')) {
+          $(this).next().fadeOut();
+      } else {
+          $('.user_accaunt_menu').not(this).hide();
+          $(this).next().fadeIn();
+      }
+  });
 
+  //Показать пароль
+  $('.shpass').on('click',function() {
+      var type = $(this).prev().attr('type') == "text" ? "password" : 'text',
+          c = $(this).html() == "<span class=\"hpass\" title=\"Скрыть пароль\"></span>" ? "<span class=\"spass\" title=\"Показать пароль\"></span>" : "<span class=\"hpass\" title=\"Скрыть пароль\"></span>";
+      $(this).html(c);
+      $(this).prev().prop('type', type);
+  });
 
+  //svg
+  //  $('#svg_increase').on('click',function() {
+  //    var hall = document.getElementById('zoom_it');
+  //    hall.setAttribute("transform", "scale(1.5)");
+  //  });
+  (function() {
+    var $section = $('#contain');
+    var $panzoom = $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find('.zoom-in'),
+    $zoomOut: $section.find('.zoom-out'),
+    $zoomRange: $section.find('.zoom-range'),
+    $reset: $section.find('.reset'),
+    cursor: 'pointer',
+    startTransform: 'scale(0.8)',
+    maxScale: 2.2,
+    increment: 0.1,
+    contain: false
+    }).panzoom('zoom', true);
+  })();
 
-//Форма логина
-$('.login_btn').click (function() {
-	$('.login').fadeIn();
-});
-$('.login_close').click (function() {
-	$('.login').fadeOut();
-});
+  $('.hall_svg').find('rect').on('click',function() {
+    location.href = "place_choose.html";
+  });
 
-$('.header_modile_login').click (function() {
-	$('.login').fadeIn();
-});
-$('.login_close').click (function() {
-	$('.login').fadeOut();
-});
+  $('.hall_svg').find('circle').on('click',function() {
+//    $(this).attr('style','fill:#F69707; stroke:#fff;');
+    if($(this).hasClass('circle_default_hover')){
+        $(this).removeClass('circle_default_hover')
+      } else {
+        $(this).addClass('circle_default_hover')
+      }
+  });
 
-$('.create_accaunt').click (function() {
-	$('#login_form').fadeOut(300);
-	$('#registration_form').delay(500).fadeIn(400);
-});
+  $(function(){
+        $("[data-original-title]").on('mousemove',function (eventObject) {
+            $data_tooltip = $(this).attr("data-original-title");
+            $("#tooltip").html($data_tooltip)
+                .css({
+                  "top" : eventObject.pageY - 470,
+                  "left" : eventObject.pageX - 265
+                })
+                .show();
+            }).mouseout(function () {
+              $("#tooltip").hide()
 
-//Переключалка табов
-(function(s) {
-    var n;
-    s(".login_position>ul").on("click", "li:not(.active)", function() {
-        n = s(this).parents(".login_position"), s(this).dmtabs(n)
-    }), s.fn.dmtabs = function(n) {
-        s(this).addClass("active").siblings().removeClass("active"), n.find("form").eq(s(this).index()).show(1, function() {
-            s(this).addClass("open_form")
-        }).siblings("form").hide(1, function() {
-            s(this).removeClass("open_form")
-        })
+        });
+  });
+
+//Анкета
+
+$('.questionnaire_select_container').on('click',function() {
+    if($(this).next().is(':visible')) {
+        $(this).next().fadeOut();
+         $(this).attr('class','questionnaire_select_container_vdwn');
+    } else {
+        $(this).next().fadeIn();
+        $(this).attr('class','questionnaire_select_container_vup');
     }
-})(jQuery);
-
-/*Мобильное меню*/
-$('.buter').click (function() {
-	$('.popup_menu').fadeIn();
-	$('#buter').attr('style', 'display: none;');
-	$('body').addClass('stop-scrolling')
-})
-$('.popup_menu_close').click (function() {
-	$('.popup_menu').fadeOut();
-	$('#buter').attr('style', 'display: block;');
-	$('body').removeClass('stop-scrolling')
-})	
-
-//Мобильный поиск
-$('.header_modile_search').click (function() {
-	$('.search').fadeIn();
-});
-$('.search_close').click (function() {
-	$('.search').fadeOut();
 });
 
-//Маска для номера телефона в инпут
-jQuery(function($) {
-$(".registration_phone_input").mask("+7 (999) 999-99-99");
-});
+//Кабинет организатора
+  //Меню
+  $('.manager_accaunt_menu_show').on('click',function() {
+      if($(this).next().is(':visible')) {
+          $(this).next().fadeOut();
+      } else {
+          $('.manager_accaunt_menu').not(this).hide();
+          $(this).next().fadeIn();
+      }
+  });
+
+  $(document).ready(function() {
+  		$('.manager_file_input').change(function() {
+  			var f_name = [];
+  			for (var i = 0; i < $(this).get(0).files.length; ++i) {
+  				f_name.push(" " + $(this).get(0).files[i].name);
+  			}
+  			$(this).next().text(f_name.join(", "));
+  		});
+  	});
+
+    //Создание события
+    $('.event_info_select_container').on('click',function() {
+        if($(this).next().is(':visible')) {
+            $(this).next().hide();
+             $(this).attr('class','event_info_select_container_vdwn');
+        } else {
+            $(this).next().show();
+            $(this).attr('class','event_info_select_container_vup');
+        }
+    });
+
+    $('.event_info_select').select2();
+
+    $('.event_info_date_time_clone').on('click',function() {
+        $("#event_info_date_time").clone().addClass('top_10').appendTo('.event_info_date_time_cloning');
+      });
+
+//Общее
+
+  //Кнопка показать календать
+  $('#calendar_btn').on('click',function() {
+    if($(this).next().is(':visible')) {
+      $(this).next().fadeOut();
+      $(this).attr('class','calendar_vdwn');
+    } else {
+      $(this).next().fadeIn();
+      $(this).attr('class','calendar_vup');
+    }
+  });
+
+  $('div.datepicker-here').datepicker({
+      // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
+      minDate: new Date()
+  });
+
+  //Плавная прокрутка в топ
+  (function($) {
+  $(function() {
+
+  	$('.page_up').click(function() {
+  		$('html, body').animate({scrollTop: 0},500);
+  		return false;
+  	})
+
+  })
+  })(jQuery)
+
+  //Временное показать еще на главной
+  $('.show_more_btn').click (function() {
+  	$('.show_more_data').fadeIn();
+  })
+
+  //Форма логина
+  $('.mobile_menu_login').click (function() {
+    $('.popup_menu').fadeOut();
+  	$('.login').fadeIn();
+    $('body').addClass('stop-scrolling');
+    $('#registration_form').fadeOut(300);
+    $('#login_form').delay(500).fadeIn(400);
+    $('#login_navigarion').addClass('active');
+    $('#registration_navigation').removeClass('active')
+  });
+  $('.login_btn').click (function() {
+  	$('.login').fadeIn();
+    $('body').addClass('stop-scrolling');
+    $('#registration_form').fadeOut(300);
+    $('#login_form').delay(500).fadeIn(400);
+    $('#login_navigarion').addClass('active');
+    $('#registration_navigation').removeClass('active')
+  });
+  $('.login_close').click (function() {
+  	$('.login').fadeOut();
+    $('body').removeClass('stop-scrolling')
+  });
+
+  $('.header_modile_login').click (function() {
+  	$('.login').fadeIn();
+    $('body').addClass('stop-scrolling');
+    $('#registration_form').fadeOut(300);
+    $('#login_form').delay(500).fadeIn(400);
+    $('#login_navigarion').addClass('active');
+    $('#registration_navigation').removeClass('active')
+  });
+  $('.login_close').click (function() {
+  	$('.login').fadeOut();
+    $('body').removeClass('stop-scrolling')
+  });
+
+  $('.create_accaunt').click (function() {
+  	$('#login_form').fadeOut(300);
+  	$('#registration_form').delay(500).fadeIn(400);
+  });
+  $('.create_accaunt_mobile_menu').click (function() {
+    $('.popup_menu').fadeOut();
+    $('.login').fadeIn();
+    $('#login_form').fadeOut(300);
+  	$('#registration_form').delay(500).fadeIn(400);
+    $('body').addClass('stop-scrolling');
+    $('#login_navigarion').removeClass('active');
+    $('#registration_navigation').addClass('active')
+  });
+
+  //Переключалка табов форма логина
+  (function(s) {
+      var n;
+      s(".login_position>ul").on("click", "li:not(.active)", function() {
+          n = s(this).parents(".login_position"), s(this).dmtabs(n)
+      }), s.fn.dmtabs = function(n) {
+          s(this).addClass("active").siblings().removeClass("active"), n.find("form").eq(s(this).index()).show(1, function() {
+              s(this).addClass("open_form")
+          }).siblings("form").hide(1, function() {
+              s(this).removeClass("open_form")
+          })
+      }
+  })(jQuery);
+
+  //Мобильное меню
+  $('.buter').click (function() {
+  	$('.popup_menu').fadeIn();
+  	$('#buter').attr('style', 'display: none;');
+  	$('body').addClass('stop-scrolling')
+  })
+  $('.popup_menu_close').click (function() {
+  	$('.popup_menu').fadeOut();
+  	$('#buter').attr('style', 'display: block;');
+  	$('body').removeClass('stop-scrolling')
+  })
+
+  //Мобильный поиск
+  $('.header_modile_search').click (function() {
+  	$('.search').fadeIn();
+    $('body').addClass('stop-scrolling')
+  });
+  $('.search_close').click (function() {
+  	$('.search').fadeOut();
+    $('body').removeClass('stop-scrolling')
+  });
+
+  //Маска для номера телефона в инпут
+  jQuery(function($) {
+    $(".registration_phone_input").mask("+7 (999) 999-99-99");
+  });
 
 });
